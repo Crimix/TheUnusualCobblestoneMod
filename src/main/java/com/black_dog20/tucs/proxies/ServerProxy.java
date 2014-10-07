64119bb3753757d8546a5d0ab0622541ddf37971
Christoffer Mouritzen <christofferm@live.dk>
2014-10-07 10:18:19 +0200

    Fixed book on server side. Done work on proxies

--- src/main/java/com/black_dog20/tucs/proxies/ServerProxy.java
+++ src/main/java/com/black_dog20/tucs/proxies/ServerProxy.java
@@ -1,11 +1,14 @@
 package com.black_dog20.tucs.proxies;
 
+import com.black_dog20.tucs.tucs;
+
 import net.minecraft.entity.player.EntityPlayer;
 
 public class ServerProxy extends CommonProxy {
 
 	@Override
 	public void openBook(EntityPlayer player) {
+		player.openGui(tucs.instance, tucs.guiIDBook, player.worldObj, (int)player.posX, (int)player.posY, (int)player.posZ);
 		
 	}
 

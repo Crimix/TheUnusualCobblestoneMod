449c81f60bf662fe9bd0e7a9f72341b612aa0909
Simon Højberg <simonhoejberg@gmail.com>
2014-10-15 20:37:06 +0200

    Added some textures and made a class

---
+++ src/main/java/com/black_dog20/tucs/item/crafting/ItemShowelHead.java
@@ -0,0 +1,14 @@
+package com.black_dog20.tucs.item.crafting;
+
+import com.black_dog20.tucs.item.ItemTUCS;
+
+public class ItemShowelHead extends ItemTUCS{
+	
+	public ItemShowelHead(){
+		
+		super();
+		this.setUnlocalizedName("showelHead");
+		
+	}
+
+}

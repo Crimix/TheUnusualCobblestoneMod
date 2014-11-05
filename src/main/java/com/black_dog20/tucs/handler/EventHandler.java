b55cc4328d9ec2a735b218c6db387b8db125fee7
Christoffer Mouritzen <christofferm@live.dk>
2014-11-05 19:26:35 +0100

    Fixed a bug

--- src/main/java/com/black_dog20/tucs/handler/EventHandler.java
+++ src/main/java/com/black_dog20/tucs/handler/EventHandler.java
@@ -92,7 +92,7 @@ public class EventHandler {
 	public void onLivingUpdatePlayer(LivingUpdateEvent event){
 		if(event.entity instanceof EntityPlayer){
 			EntityPlayer player = (EntityPlayer) event.entity;
-			if(player.capabilities.allowFlying){
+			if(player.capabilities.allowFlying && !player.capabilities.isCreativeMode){
 			if(player.capabilities.allowFlying && !(player.inventory.hasItemStack(new ItemStack(ModItems.FlightTalisman)))){
 				player.capabilities.allowFlying = false;
 				player.capabilities.isFlying = false;
